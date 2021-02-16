6d1a680fe2d3fdc3e20c6a1eefc4579f3a7246d8
Denis Solovev <dsolovev@tutanota.com>
2021-02-16 03:19:42 -0500

    add solving code

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main(int /*argc*/, char const */*argv*/[])
 {
-    /* code */
-    return 0;
+    int a, b, f;
+    std::cout << "vvedite chisla a, b, f: ";
+    std::cin >> a >> b >> f;
+    std::cout << "x = (а + b — f / а) + f * a * a — (a + b) = "
+              << (a + b - f / a) + f * a * a - (a + b) << '\n';
 }
